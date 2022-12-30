f690cc113302b506411a790341efcd26205512e1
kaushik6002 <29anubhav29@gmail.com>
2022-12-30 12:12:39 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+
+//Steps involved in the setup:
+// npm init
+// fill in the details like entry point and description
+// and package.json will be created
+//git init (to initialise a repository)
+//add a .gitignore(touch .gitignore) and include node_modules and .env files
+//intall dependencies(these will be installed in package.json)
+//dependencies: express mongoose dotenv bcrypt(to hash passwords) colors(for terminal)
+//nodemon as dev dependecy(npm i -D nodemon)
+//create a script for nodemon("server":"nodemon backend/server.js")
+
+
+
+
+// server.js is the entry point for the backend
+console.log("Server running...");
